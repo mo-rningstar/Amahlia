fbb86a7e76584abd96017bc4f8ded2a945e8255e
Amahlia Su <emilytianyisu@gmail.com>
2017-08-10 11:19:32 -0500

    Upload

---
+++ aboutme.js
@@ -0,0 +1,18 @@
+function aboutme() {
+	window.open("aboutme.html");
+}
+function home() {
+	window.open("home.html");
+}
+function contact() {
+	window.open("contact.html");
+}
+function fun() {
+	window.open("mybootstrapsite.html");
+}
+
+
+
+
+      
+
