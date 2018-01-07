bf1b8fe1a3ecb5f894b214a31af46cb34eefcea8
Amahlia Su <emilytianyisu@gmail.com>
2018-01-06 21:44:04 -0600

    update 1.1

--- a.js
+++ a.js
@@ -1,14 +1,14 @@
 function home() {
-	window.open("index.html");
+	window.open("index.html", "_self");
 }
 function aboutme() {
-	window.open("1.html");
+	window.open("1.html", "_self");
 }
 function contact() {
-	window.open("2.html");
+	window.open("2.html", "_self");
 }
 function fun() {
-	window.open("3.html");
+	window.open("3.html", "_self");
 }
 
 
